001234511c0b7e2a3818e829fcfa3b683010446a
nolanheriniaina <nolanheriniaina.dev@gmail.com>
2026-04-20 16:28:36 +0300

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: 'https://raatook.mg', // Remplacez par votre URL
-    generateRobotsTxt: true, // génère un fichier robots.txt
-    sitemapSize: 5000, // optionnel : nombre d'URLs par sitemap
-    changefreq: 'daily', // optionnel : fréquence de changement
-    priority: 0.7, // optionnel : priorité des pages
-    exclude: [], // optionnel : pages à exclure
+    siteUrl: 'https://nolanheriniaina.mg',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
+    exclude: ['/api/*'],
+
+    additionalPaths: async (config) => {
+        const locales = ['en', 'fr']
+
+        return locales.map((locale) => ({
+            loc: `/${locale}`,
+            changefreq: config.changefreq,
+            priority: config.priority,
+            lastmod: new Date().toISOString(),
+        }))
+    },
 }
 
 module.exports = config
